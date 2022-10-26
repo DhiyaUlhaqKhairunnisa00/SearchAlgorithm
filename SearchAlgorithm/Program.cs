e64d71c20cf20102ba894d0e893b3b2ece6d5743
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2022-10-26 16:06:26 +0700

    add int

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+
+namespace SearchAlgorithm
+{
+    class Program
+    { 
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+
+}
