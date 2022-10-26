b7a6dbab56fa62539a5cb75b6f1e7d073f65cb92
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2022-10-26 16:39:49 +0700

    add method binaryseacrh

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -3,7 +3,7 @@
 namespace SearchAlgorithm
 {
     class Program
-    { 
+    {
         //Array to be searched
         int[] arr = new int[20];
         //Number of elements in the array
@@ -30,10 +30,55 @@ namespace SearchAlgorithm
             Console.WriteLine("--------------------");
             for (i = 0; i < n; i++)
             {
-                Console.Write("<"+(i+1)+">");
+                Console.Write("<" + (i + 1) + ">");
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
             }
+        }
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                //Accept the number to be searched
+                Console.Write
+                    ("\nEnter element want you to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
 
+                //Apply binary search
+                int lowerbound = 0;
+                int upperbound = 0;
+
+                //obtain the index of the middle elements
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position"
+                        + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString()
+                        + "not found in the array\n");
+                Console.WriteLine("\nNumber of comparison :" + ctr);
+
+                Console.Write("\nContinue search (y/n):");
+                ch = char.Parse(Console.ReadLine().ToUpper());
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
+        static void Main(string[] args)
+        {
 
         }
+    }
+}    
