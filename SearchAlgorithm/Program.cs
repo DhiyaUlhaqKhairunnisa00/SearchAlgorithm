e23471b72f049ed082819b5ce8fad048605a2e40
DhiyaulhaqKhairunnisa00 <dhiyacacaa00@gmail.com>
2022-10-26 17:29:02 +0700

    add method to run the program

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -18,7 +18,7 @@ namespace SearchAlgorithm
                 Console.Write("Enter the number of elements in the array");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
-                if ((n < 0) && (n <= 20))
+                if ((n > 0) && (n <= 20))
                     break;
                 else
                     Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements. \n");
@@ -109,7 +109,57 @@ namespace SearchAlgorithm
         }
         static void Main(string[] args)
         {
+            Program myList = new Program();
+            int pilihanmenu;
+            char ch;
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Menu Option");
+                    Console.WriteLine("--------------");
+                    Console.WriteLine("1. Linear Search");
+                    Console.WriteLine("2. Binary Search");
+                    Console.WriteLine("3. Exit");
+                    Console.Write("Enter your Choice ( 1,2,3) : ");
+                    pilihanmenu = Convert.ToInt32(Console.ReadLine());
+                    switch (pilihanmenu)
+                    {
+                        case 1:
+                            Console.WriteLine("");
+                            Console.WriteLine(". . . . . . . . . . . . .");
+                            Console.WriteLine("linear Search");
+                            Console.WriteLine(". . . . . . . . . . . . .");
+                            myList.input();
+                            myList.LinearSearch();
+                            break;
 
+                        case 2:
+                            Console.WriteLine("");
+                            Console.WriteLine(". . . . . . . . . . . . .");
+                            Console.WriteLine("Binary Search");
+                            Console.WriteLine(". . . . . . . . . . . . .");
+                            myList.input();
+                            myList.BinarySearch();
+                            break;
+
+                        case 3:
+                            Console.WriteLine("exit.");
+                            break;
+                        default:
+                            Console.WriteLine("Error");
+                            break;
+                    }
+                    Console.WriteLine("\nPilih Menu Lagi? (y/n) : ");
+                    ch = char.Parse(Console.ReadLine().ToLower());
+                    Console.Clear();
+                } while (ch == 'y');
+
+                // to exit from the console
+                Console.WriteLine("\n\nPress Return to exit. ");
+
+                Console.ReadLine();
+            } while(pilihanmenu != 3);    
         }
     }
 }    
